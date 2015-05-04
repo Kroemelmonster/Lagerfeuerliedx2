0d2a1c05259007dcb401067b8cb2780e1a794e18
Kroemelmonster <PHouligans@web.de>
2015-05-04 12:48:26 +0200

    Meilenstein 3 Aufgabe 1
    Erster Versuch. Muss local hosted werden. Habe kein File read genommen
    oder die json datei in eine js umgewandelt. Auch wenn ich die json datei
    umwandlen musste da sie Fehler hatte...

---
+++ Meilenstein3/book.js
@@ -0,0 +1,68 @@
+function onload(){
+	//mit horror begint es...
+    horror();
+}
+
+function sendRequest(filename,name) {
+	// funktioniert also nicht mit read file oder son käse... musss localhosted werden
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status== 200) {
+			console.log(xmlhttp.responseText);
+			var json = JSON.parse(xmlhttp.responseText);
+			console.log(json);
+			load(json[name]);
+		}
+	}
+	xmlhttp.open("GET",filename);
+	xmlhttp.send();
+}
+
+function horror(){
+	// swap die class names.
+    var o1 = document.getElementById("horror");
+    o1.className = "blueborder bluebutton buttonbox selected";
+    
+    var o2 = document.getElementById("roman");
+    o2.className = "blueborder bluebutton buttonbox";
+	sendRequest("horror_books.json","horrordata");
+}
+
+function roman(){
+	// swap die class names.
+    var o1 = document.getElementById("horror");
+    o1.className = "blueborder bluebutton buttonbox";
+    
+    var o2 = document.getElementById("roman");
+    o2.className = "blueborder bluebutton buttonbox selected";
+	sendRequest("roman_books.json","romandata");
+}
+
+function load(json) {
+	// erstelle den tabellen inhalt html anhand der json object datei
+	var table = "";
+	table += '<tr>';
+	table += '<th>Autor</th>';
+	table += '<th>Titel</th>';
+	table += '<th>Kapitel</th>';
+	table += '<th>Buchart</th>';
+	table += '<th>ISBN</th>';
+	table += '<th>Erscheinungjahr</th>';
+	table += '<th>Auflage</th>';
+	// nachfolgende geht auch es nimmt halt einfach nur die keys. die wiederum leider lowcase sind
+1	/*Object.keys(json[0]).forEach(function(k) {
+		table += '<th>'+k+'</th>';
+	});*/
+	table += '</tr>';
+	for (index = 0; index < json.length; ++index) {
+		table += '<tr>';
+		for(var entry in json[index]) { 
+			table += '<td>'+json[index][entry];
+			if (entry == "auflage")
+				table += ". Auflage";
+			table += '</td>';
+		}
+		table += '</tr>';
+	}
+    document.getElementById("table").innerHTML = table;
+}
